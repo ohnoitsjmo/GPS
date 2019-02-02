412f69c35dcdf517dabd643e13ef4992427363bb
ohnoitsmo <justin1998mo@gmail.com>
2019-02-01 20:36:06 -0800

    feature/style

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateAllIncidents() {
+    this.router.navigate(['/all_incidents']);
+  }
+
+  navigateMapVisual() {
+    this.router.navigate(['/map_visual']);
   }
 
 }
