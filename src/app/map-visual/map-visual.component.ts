3c7a6ab0688ae9f1383bdd6bd57a5f461f4f4d0e
ohnoitsmo <justin1998mo@gmail.com>
2019-02-02 02:16:01 -0800

    data-visuals

--- src/app/map-visual/map-visual.component.ts
+++ src/app/map-visual/map-visual.component.ts
@@ -11,6 +11,7 @@ export class MapVisualComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var divElement = document.getElementById('viz1547622640092');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                              
+    var divElement = document.getElementById('viz1549102256633');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                
+
   }
 }
