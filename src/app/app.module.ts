dfe5d10ceb6ba041f45aa568d0df067da360741c
ohnoitsmo <justin1998mo@gmail.com>
2019-02-01 20:26:41 -0800

    feature/server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { AllIncidentsComponent } from './all-incidents/all-incidents.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
+import { DataTableModule } from "angular-6-datatable";
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+import { AllIncidentsComponent } from './all-incidents/all-incidents.component';
 import { MapVisualComponent } from './map-visual/map-visual.component';
+
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'map_visual',
+    component: MapVisualComponent
+  },
+  {
+    path: 'all_incidents',
+    component: AllIncidentsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AllIncidentsComponent,
-    FooterComponent,
     HeaderComponent,
+    FooterComponent, 
+    AppComponent,
     HomeComponent,
+    AllIncidentsComponent,
     MapVisualComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    DataTableModule,
+    AgmSnazzyInfoWindowModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBkxgq_lARNwLFmykPNIef6TaSEDp6OarY'
+    })
   ],
-  providers: [],
+  entryComponents : [
+  ],
+  providers: [
+    GoogleMapsAPIWrapper,
+  ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
