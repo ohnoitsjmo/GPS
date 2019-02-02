f8430930bcf0552788099384121107f64b126e70
ohnoitsmo <justin1998mo@gmail.com>
2019-02-01 20:11:19 -0800

    feature/components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AllIncidentsComponent } from './all-incidents/all-incidents.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MapVisualComponent } from './map-visual/map-visual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllIncidentsComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    MapVisualComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/map-visual/map-visual.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-visual',
+  templateUrl: './map-visual.component.html',
+  styleUrls: ['./map-visual.component.css']
+})
+export class MapVisualComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
