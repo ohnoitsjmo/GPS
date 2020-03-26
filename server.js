6bd8b43d007fa6bf340050df0c524ec000b20599
Elaine Pranadjaya <elainesabine@gmail.com>
2020-03-26 03:20:44 -0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -80,3 +80,20 @@ app.get('/queryActiveIncidents.json', function(req, res) {
 
 app.listen(3000);
 console.log("Server running on port 3000");
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
